e8cd82e38bc9a8f85f02ded4d8f9324255023b8f
mohitraj1050 <rajmohit805179@gmail.com>
2021-04-23 01:56:47 +0530

    dict

---
+++ venv/If_else/solution3.py
@@ -0,0 +1,10 @@
+# Write a python program to check the user input abbreviation.If the user enters "lol", print "laughing out loud".If the user enters "rofl", print "rolling on the floor laughing".If the user enters "lmk", print "let me know".If the user enters "smh", print "shaking my head".
+s = int(input());
+if s == lol:
+    print("laughing loud lot")
+elif s == rofl:
+    print("rolling on the floor laughing")
+elif s == lmk:
+    print("let me know")
+elif s == smh:
+    print("shaking my head")
