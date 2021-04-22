d5eb573596d0d1452531fe0de7b004daf08efc56
mohitraj1050 <rajmohit805179@gmail.com>
2021-04-23 02:10:38 +0530

    if_el

---
+++ venv/If_else/solution2.py
@@ -0,0 +1,7 @@
+number1 = input("Enter number and hit enter ")
+print("Printing type of input value")
+print("type of number ", type(number1))
+if(type(number1) == float):
+	print('This is a float')
+else:
+	print('Not a float')
